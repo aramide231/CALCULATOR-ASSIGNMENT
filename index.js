f072be640d6d30598f1d698f6536e4e5271dd133
Araba Aramide <aramide231@gmail.com>
2023-08-30 18:01:11 +0100

    added button values

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 document.getElementById("btn").addEventListener("click",function(){
-alert("button clicked")})
+console.log("button clicked")})
+
+    document.getElementById("one").addEventListener("click", function() {
+        console.log("1");
+    });
+
+    document.getElementById("two").addEventListener("click", function() {
+        console.log("2");
+    });
+
+    document.getElementById("three").addEventListener("click", function() {
+        console.log("3");
+    });
+
+    document.getElementById("four").addEventListener("click", function() {
+        console.log("4");
+    });
+
+    document.getElementById("five").addEventListener("click", function() {
+        console.log("5");
+    });
+
+    document.getElementById("six").addEventListener("click", function() {
+        console.log("6");
+    });
+
+    document.getElementById("seven").addEventListener("click", function() {
+        console.log("7");
+    });
+
+    document.getElementById("eight").addEventListener("click", function() {
+        console.log("8");
+    });
+
+    document.getElementById("nine").addEventListener("click", function() {
+        console.log("9");
+    });
+
+
+
+
   
