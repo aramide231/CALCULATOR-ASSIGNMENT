83bfe73654dff911b0318319aa2cf31daaf0463f
aramide231 <137628186+aramide231@users.noreply.github.com>
2023-09-05 07:50:02 +0000

    made another change it should work now

--- index.js
+++ index.js
@@ -84,44 +84,44 @@ squareRoot.addEventListener("click", function () {
 
 
 
-document.getElementById("btn").addEventListener("click",function(){
-console.log("button clicked")})
+// document.getElementById("btn").addEventListener("click",function(){
+// console.log("button clicked")})
 
-    document.getElementById("one").addEventListener("click", function() {
-        console.log("1");
-    });
+//     document.getElementById("one").addEventListener("click", function() {
+//         console.log("1");
+//     });
 
-    document.getElementById("two").addEventListener("click", function() {
-        console.log("2");
-    });
+//     document.getElementById("two").addEventListener("click", function() {
+//         console.log("2");
+//     });
 
-    document.getElementById("three").addEventListener("click", function() {
-        console.log("3");
-    });
+//     document.getElementById("three").addEventListener("click", function() {
+//         console.log("3");
+//     });
 
-    document.getElementById("four").addEventListener("click", function() {
-        console.log("4");
-    });
+//     document.getElementById("four").addEventListener("click", function() {
+//         console.log("4");
+//     });
 
-    document.getElementById("five").addEventListener("click", function() {
-        console.log("5");
-    });
+//     document.getElementById("five").addEventListener("click", function() {
+//         console.log("5");
+//     });
 
-    document.getElementById("six").addEventListener("click", function() {
-        console.log("6");
-    });
+//     document.getElementById("six").addEventListener("click", function() {
+//         console.log("6");
+//     });
 
-    document.getElementById("seven").addEventListener("click", function() {
-        console.log("7");
-    });
+//     document.getElementById("seven").addEventListener("click", function() {
+//         console.log("7");
+//     });
 
-    document.getElementById("eight").addEventListener("click", function() {
-        console.log("8");
-    });
+//     document.getElementById("eight").addEventListener("click", function() {
+//         console.log("8");
+//     });
 
-    document.getElementById("nine").addEventListener("click", function() {
-        console.log("9");
-    });
+//     document.getElementById("nine").addEventListener("click", function() {
+//         console.log("9");
+//     });
 
 
 
